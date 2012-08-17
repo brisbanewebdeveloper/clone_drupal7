9c3b00c4c6b7d371d50bc5be3c413cef07878052
hironozu <contact@hironozu.com>
2012-08-18 08:44:53 +1000

    To use same branch to clone modules.

--- clone.sh
+++ clone.sh
@@ -8,61 +8,48 @@ fi
 
 
 
+
+drupal=$1
+branch=7.x-1.x
+
 # Core
-if [ ! -d "$1" ]
+if [ ! -d "$drupal" ]
 then
-    git clone --recursive -b 7.x http://git.drupal.org/project/drupal.git $1
-    if [ ! -d "$1" ]; then
-        echo Drupal directory $1 does not exist
+    git clone --recursive -b $branch http://git.drupal.org/project/drupal.git $drupal
+    if [ ! -d "$drupal" ]; then
+        echo Drupal directory $drupal does not exist
         exit 0
     fi
-    echo "sites/all" >> $1/.gitignore
-    cd $1
+    echo "sites/all" >> $drupal/.gitignore
+    cd $drupal
     git add .gitignore
     git commit -m 'Updating .gitignore'
     cd ..
 else
-    echo Drupal directory $1 exists.
+    echo Drupal directory $drupal exists.
 fi
 
 
 # Modules
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 filecontent=( `cat "$DIR/modules.txt"` )
-for t in "${filecontent[@]}"
+for module in "${filecontent[@]}"
 do
-    if [ -n "$t" ]
+    if [ -n "$module" ]
     then
-
-        # Check if branch is specified
-        if [[ "$t" =~ : ]]
-        then
-            arr=$(echo $t | tr ":" "\n")
-            arr=( `echo $arr` ) # Somehow $arr does not get splited properly on my iMac without this
-            module=${arr[0]}
-            branch=${arr[1]}
-        else
-            module=$t
-        fi
-
-        if [ -d "$1/sites/all/modules/$module" ]
+        if [ -d "$drupal/sites/all/modules/$module" ]
         then
             echo Module directory $module exists
         else
-            if [ -n "$branch" ]
-            then
-                git clone --recursive -b $branch http://git.drupal.org/project/$module.git $1/sites/all/modules/$module
-            else
-                git clone --recursive http://git.drupal.org/project/$module.git $1/sites/all/modules/$module
-            fi
+            git clone --recursive -b $branch http://git.drupal.org/project/$module.git $drupal/sites/all/modules/$module
         fi
     fi
 done
 
 
 # Theme
-if [ -d "$1/sites/all/themes/omega" ]
+if [ -d "$drupal/sites/all/themes/omega" ]
 then
-    git clone --recursive http://git.drupal.org/project/omega.git $1/sites/all/themes/omega
+    git clone --recursive http://git.drupal.org/project/omega.git $drupal/sites/all/themes/omega
 fi
 
